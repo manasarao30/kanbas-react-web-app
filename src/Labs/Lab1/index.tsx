fb0e6e459fcc51c30559d65353df6cde95f6c20f
manasarao54747@gmail.com <manasarao54747@gmail.com>
2024-07-06 18:30:51 -0400

    Fixed Image Tags

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -158,12 +158,13 @@ Your favorite books (in no particular order)
   <br />
   <img id="wd-starship"
     width="400px"
-   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" 
+   alt="Starship"
   />
   <br />
   Loading a local image:
   <br />
-  <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+  <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" alt="Tesla Bot" />
 </div>
 
 </div>
