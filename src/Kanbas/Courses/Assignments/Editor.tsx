12f700f0049b75712c1dcf448379357aef3150e8
manasarao54747@gmail.com <manasarao54747@gmail.com>
2024-07-07 14:51:50 -0400

    implemented half assignment editor

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -1,10 +1,14 @@
 export default function AssignmentEditor() {
     return (
       <div id="wd-assignments-editor">
-        <label htmlFor="wd-name">Assignment Name</label>
+        <label htmlFor="wd-name">Assignment Name</label> <br></br>
         <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
         <textarea id="wd-description">
-          The assignment is available online Submit a link to the landing page of
+        The assignment is available online.Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following: Your full name and section
+        Links to each of the lab assignments
+        Link to the Kanbas application
+        Links to all relevant source code repositories
+        The Kanbas application should include a link to navigate back to the landing page.
         </textarea>
         <br />
         <table>
@@ -49,11 +53,30 @@ export default function AssignmentEditor() {
             <td align="right" valign="top">
               <label htmlFor="wd-submission-type">Submission Type</label>
             </td>
+
             <td>
             <select id="wd-submission-type">
             <option selected value="Online">Online</option>
             <option value="In Class">In Class</option>
             </select>
+            </td>
+    
+            <td>
+            <label>Online Entry Options</label><br/>
+                <input type="checkbox" name="wd-text-entry" id="wd-text-entry"/>
+                <label htmlFor="wd-text-entry">Text Entry</label><br/>
+
+                <input type="checkbox" name="wd-website-url" id="wd-website-url"/>
+                <label htmlFor="wd-website-url">Website URL</label><br/>
+
+                <input type="checkbox" name="wd-media-recordings" id="wd-media-recordings"/>
+                <label htmlFor="wd-media-recordings">Media Recordings</label><br/>
+
+                <input type="checkbox" name="wd-student-annotation" id="wd-student-annotation"/>
+                <label htmlFor="wd-student-annotation">Student Annotation</label>
+
+                <input type="checkbox" name="wd-file-upload" id="wd-file-upload"/>
+                <label htmlFor="wd-file-upload">File Uploads</label>
             </td>
           </tr>
 
